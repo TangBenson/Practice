ae2333b103439f63b339fe38509603ae134a7134
TangWeiChi <nerodmd@gmail.com>
2023-10-14 17:39:19 +0800

    golang2

--- golang/hello.go
+++ golang/hello.go
@@ -45,12 +45,33 @@ func main() {
 	// for q = 0; q < 3; q++ {
 	// 	fmt.Println("AAA")
 	// }
+
+	// fmt.Println("-----------------------------")
+
 	Show(10)
 	var xx int = Mutiply(3, 4)
+	yy := Mutiply(3, 4) // :=是語法糖，yy和xx一樣
 	fmt.Println(xx)
-	var sq int
-	var sw string
-	sq, sw = Test(5, 2)
+	fmt.Println(yy)
+	var x int
+	var s string
+	x, s = Test(5, 2)
+	fmt.Println(x, s)
+	Test2()
+
+	// fmt.Println("-----------------------------")
+
+	// Pointer用來存記憶體位置
+	// var x int = 3
+	// fmt.Println(&x)    //取得記憶體位置
+	// var xPtr *int = &x //建立Pointer指標變數
+	// fmt.Println(xPtr)
+	// fmt.Println(*xPtr) //反解Pointer
+
+	// fmt.Println("-----------------------------")
+
+	// var p1 Person = Person{"柔柔", 18}
+	// fmt.Println(p1.age, p1.name)
 }
 
 func Show(max int) {
@@ -59,7 +80,7 @@ func Show(max int) {
 	for n = 1; n < max; n++ {
 		result += n
 	}
-	fmt.Println(max)
+	fmt.Println(result)
 }
 
 func Mutiply(n1 int, n2 int) int {
@@ -69,4 +90,20 @@ func Mutiply(n1 int, n2 int) int {
 
 func Test(n1 int, n2 int) (int, string) {
 	return n1 * n2, "ddd"
+}
+
+func Test2() {
+	//defer 會等到周圍的 function 都執行完成後，再執行
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
+
+type Person struct {
+	name string
+	age  int
 }
