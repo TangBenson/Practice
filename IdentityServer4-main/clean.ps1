b5922dcf4fb0211c983d8c078ffe4ab62cfe1bc6
BensonTang <benson922@hotaimotor.com.tw>
2023-10-25 14:17:00 +0800

    id4專案刪掉

--- IdentityServer4-main/clean.ps1
+++
@@ -1,5 +0,0 @@
-# 从工作目录中删除未跟踪的文件和目录。但排除了 samples 目录和 src/IdentityServer4/.vs 目录，不会删除这些目录中的文件
-git clean -xdf -e samples -e src/IdentityServer4/.vs
-
-# 运行名为 "clean_cache.ps1" 的 PowerShell 脚本
-./clean_cache.ps1
--- IdentityServer4-main/create_persisted_grant_store.sh
+++
@@ -1,5 +0,0 @@
-# 用于为 IdentityServer4 项目的持久化授权数据库创建一个新的数据库迁移
-# -c指定了DbContext 类的名称
-# -o指定了生成的迁移文件的输出目录
-# -p指定了用于创建迁移的项目文件
-dotnet ef migrations add InitialIdentityServerPersistedGrantDbMigration -c PersistedGrantDbContext -o Data/Migrations/IdentityServer/PersistedGrantDb -p src/IdentityServer4/host/Host.csproj
